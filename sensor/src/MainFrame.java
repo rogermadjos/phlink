de084311fcc84af62cc2f87312ee1d7fbfdccf2c
Roger Madjos <roger.madjos@gmail.com>
2016-05-14 23:45:09 +0800

    feat(sensore): qrcode reader

--- sensor/src/MainFrame.java
+++ sensor/src/MainFrame.java
@@ -1,3 +1,4 @@
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
@@ -8,12 +9,16 @@ import java.awt.image.BufferedImage;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+
+import sun.net.www.protocol.gopher.GopherClient;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class MainFrame extends JFrame implements ActionListener {
-    private JPanel contentPane;
+public class MainFrame extends JFrame {
+    private ImagePanel videoPane;
     public final int FRAME_WIDTH = 640;
     public final int FRAME_HEIGHT = 480;
     
@@ -21,8 +26,7 @@ public class MainFrame extends JFrame implements ActionListener {
     private QRDecoder qrDecoder;
     private Thread displayThread;
     private Thread decodeThread;
-    private JLabel userDisplay;
-    private JLabel statusDisplay;
+    private Indicator indicator;
 
     /**
     * Launch the application.
@@ -44,6 +48,8 @@ public class MainFrame extends JFrame implements ActionListener {
     * Create the frame.
     */
     public MainFrame() {
+    	super();
+    	logger.setLevel(Level.INFO);
     	setTitle("phlink");
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
@@ -56,32 +62,46 @@ public class MainFrame extends JFrame implements ActionListener {
 			@Override
 			public void windowClosed(WindowEvent e) {
 				logger.log(Level.INFO, "close");
+				((StoppableThread) displayThread).stopThread();
+				((StoppableThread) decodeThread).stopThread();
 				videoCap.release();
-				displayThread.stop();
-				decodeThread.stop();
 			}
 		});
         
-        contentPane = new JPanel();
-        contentPane.setLayout(null);
+        videoPane = new ImagePanel();
+        videoPane.setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
+        
+        indicator = new Indicator();
         
         GroupLayout layout = new GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
+        this.setLayout(layout);
         layout.setAutoCreateGaps(true);
         layout.setAutoCreateContainerGaps(true);
 
-        layout.setHorizontalGroup(layout.createParallelGroup()
-        	.addComponent(contentPane, FRAME_WIDTH, FRAME_WIDTH, FRAME_WIDTH)
+        layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER)
+        	.addComponent(indicator, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+        	.addComponent(videoPane, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+        	
         );
         
         layout.setVerticalGroup(layout.createSequentialGroup()
-        	.addComponent(contentPane, FRAME_WIDTH, FRAME_WIDTH, FRAME_WIDTH)
+        	.addComponent(indicator, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+        	.addComponent(videoPane, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
         );
         
         pack();
         setLocationRelativeTo(null);
         
-        qrDecoder = new QRDecoder(this);
+        qrDecoder = new QRDecoder(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (e.getActionCommand() == "QR_CODE_ACTIVE") {
+					logger.log(Level.INFO, "QR_CODE_ACTIVE");
+					indicator.setState(Indicator.ACTIVE);
+				}
+			}
+		});
 
         displayThread = new DisplayThread();
         decodeThread = new DecodeThread();
@@ -95,28 +115,32 @@ public class MainFrame extends JFrame implements ActionListener {
     private BufferedImage getFrame() {
     	return videoCap.getOneFrame();
     }
-
-    public void paint(Graphics g){
-    	g = contentPane.getGraphics();
-    	imageFrame = getFrame();
-    	g.drawImage(imageFrame, 0, 0, this);
+    
+    class StoppableThread extends Thread {
+    	protected boolean active = true;
+    	
+        public void stopThread() {
+        	active = false;
+		}
     }
 
-    class DisplayThread extends Thread {
+    class DisplayThread extends StoppableThread {
         @Override
         public void run() {
-            for (;;){
-                repaint();
+            while (active){
+                BufferedImage image = getFrame();
+                imageFrame = image;
+                videoPane.updateImage(imageFrame);
                 try { Thread.sleep(20);
                 } catch (InterruptedException e) {    }
             }  
         } 
     }
     
-    class DecodeThread extends Thread {
+    class DecodeThread extends StoppableThread {
         @Override
         public void run() {
-            for (;;){
+            while (active){
             	if (imageFrame != null) {
                 	synchronized (imageFrame) {
                 		qrDecoder.decodeQRCode(imageFrame);
@@ -125,13 +149,7 @@ public class MainFrame extends JFrame implements ActionListener {
                 try { Thread.sleep(100);
                 } catch (InterruptedException e) {    }
             }  
-        } 
+        }
     }
-
-	public void actionPerformed(ActionEvent e) {
-		if (e.getActionCommand() == "QR_CODE") {
-			logger.log(Level.INFO, qrDecoder.getData());
-		}
-	}
 
 }
--- sensor/src/QRDecoder.java
+++ sensor/src/QRDecoder.java
@@ -4,7 +4,11 @@ import java.awt.image.BufferedImage;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.swing.Timer;
+
 import com.google.zxing.BinaryBitmap;
+import com.google.zxing.ChecksumException;
+import com.google.zxing.NotFoundException;
 import com.google.zxing.ReaderException;
 import com.google.zxing.Result;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
@@ -15,9 +19,23 @@ public class QRDecoder {
 	private final Logger logger = Logger.getLogger(QRDecoder.class.getName());
 	private ActionListener listener;
 	private String data = "";
+	private boolean canDecode = true;
 	
 	public QRDecoder(ActionListener listener) {
+		logger.setLevel(Level.INFO);
 		this.listener = listener;
+	}
+	
+	private synchronized void debounce() {
+		canDecode = false;
+		Timer timer = new Timer(3000, new ActionListener() {
+			@Override
+			public synchronized void actionPerformed(ActionEvent e) {
+				canDecode = true;
+			}
+		});
+		timer.setRepeats(false);
+		timer.start();
 	}
 	
 	public String getData() {
@@ -25,20 +43,27 @@ public class QRDecoder {
 	}
 	
     public void decodeQRCode(BufferedImage image) {
+    	if(!canDecode) {
+    		return;
+    	}
     	BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(image)));
     	QRCodeReader reader = new QRCodeReader();
-    	
     	Result result;
     	try {
 			result = reader.decode(binaryBitmap);
 			String newData = result.getText();
-			if(!newData.equals(data)) {
-				data = newData;
-				listener.actionPerformed(new ActionEvent(this, 0, "QR_CODE"));
-			}
-//    		logger.log(Level.INFO, newData + ":" + data);
+			data = newData;
+			debounce();
+			listener.actionPerformed(new ActionEvent(this, 0, "QR_CODE_ACTIVE"));
+    		logger.log(Level.FINE, "new: " + newData + ", old: " + data);
 		} catch (ReaderException e) {
-//			logger.log(Level.INFO, "No QR code", e);
+			if( e instanceof ChecksumException ) {
+				listener.actionPerformed(new ActionEvent(this, 0, "QR_CODE_AWARE"));
+			}
+			if( e instanceof NotFoundException ) {
+				listener.actionPerformed(new ActionEvent(this, 0, "QR_CODE_IDLE"));
+			}
+			logger.log(Level.FINEST, "No QR code", e);
 		}
     }
 }
